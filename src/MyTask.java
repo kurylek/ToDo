e8d114b9d719ddde62cfb23804c8f2540673cd1f
kurylek <kurylek98@gmail.com>
2021-06-16 00:34:52 +0200

    MyTask & MyTaskStatus basics

---
+++ src/MyTask.java
@@ -0,0 +1,51 @@
+public class MyTask {
+    private String name;
+    private String description;
+    private MyTaskStatus status;
+
+    public MyTask(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.status = MyTaskStatus.ACTIVE;
+    }
+
+    public MyTask(String name) {
+        this.name = name;
+        this.status = MyTaskStatus.ACTIVE;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public MyTaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(MyTaskStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+            String desc;
+        if (description.length() > 18)
+            desc = description.substring(0, 15) + "...";
+        else
+            desc = description;
+
+        return name + "- " + desc;
+    }
+}
