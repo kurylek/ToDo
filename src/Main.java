ee766cfba9150526aa20dc21413f1ca64828fb8c
kurylek <kurylek98@gmail.com>
2021-06-16 23:30:57 +0200

    Basic scene

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
     public static void main(String[] args) {
-	// write your code here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("fxml/mainScene.fxml"));
+
+        stage.setScene(new Scene(fxmlLoader.load()));
+        stage.setTitle("ToDo");
+        stage.show();
     }
 }
